c31aa1c390b3417d6a62636d7e2caa66a767017b
georgeven <georgevenven@gmail.com>
2021-12-23 13:07:20 -0800

    some tings

--- main.py
+++ main.py
@@ -12,3 +12,15 @@ app.config['SECRET_KEY'] = '42' #pass secret key through file private on github
 @app.route('/')
 def index():
     return render_template('home.html')
+
+@app.route('/writings')
+def writings():
+    return render_template('writings.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/projects')
+def projects():
+    return render_template('projects.html')
