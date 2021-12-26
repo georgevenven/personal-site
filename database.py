757d96a13bdabec5f46dead23426e1a99a6012ac
georgeven <georgevenven@gmail.com>
2021-12-25 22:59:09 -0800

    database

--- database.py
+++ database.py
@@ -1,8 +1,13 @@
 #initates the database file
-
+#running the database file should update all the 
+#entries on the site because all of the stuff should 
+#be stored locally as well
 import sqlite3
 
 connection = sqlite3.connect('database.db')
+
+htmlfile = open("test1.txt", "r")
+print(htmlfile.read())
 
 with open('schema.sql') as f:
     connection.executescript(f.read())
